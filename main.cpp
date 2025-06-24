c4665321d1b07e41fd6ed117a25cf8b48e4275f3
OffworldAstronaut <rafael.science.amorim@gmail.com>
2025-06-24 10:33:40 -0300

    feat: código operacional

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@
 #include <random>   // números aleatórios
 #include <format>   // formatação de strings
 
-#define PI 3.1415926535 // PI
-#define I complex<double>(0.0, 1.0)
+#define PI 3.1415926535                 // PI
+#define I complex<double>(0.0, 1.0)     // Unidade imaginária (I)
 
 using namespace std;
 
@@ -33,6 +33,10 @@ class Cristal
 {
 public:
     vector<complex<double>> psi_n;  // vetor representativo do valor da função de onda em cada sítio
+    vector<complex<double>> k1;     // vetor para os valores de k1 do runge kutta
+    vector<complex<double>> k2;     // vetor para os valores de k2 do runge kutta 
+    vector<complex<double>> k3;     // vetor para os valores de k3 do runge kutta 
+    vector<complex<double>> k4;     // vetor para os valores de k4 do runge kutta 
     vector<double> pot;             // vetor de potenciais do cristal (on-site energy)
     float W;                        // largura de desordem do sistema
     float N;                        // tamanho da cadeia cristalina
@@ -46,6 +50,10 @@ public:
      */
     Cristal(int tam_cristal, float W_param, float sigma)
         :   psi_n(tam_cristal, complex<double>(0.0, 0.0)),  // função de onda zerada
+            k1(tam_cristal, complex<double>(0.0, 0.0)),     // k1 zerado 
+            k2(tam_cristal, complex<double>(0.0, 0.0)),     // k2 zerado 
+            k3(tam_cristal, complex<double>(0.0, 0.0)),     // k3 zerado 
+            k4(tam_cristal, complex<double>(0.0, 0.0)),     // k4 zerado
             pot(tam_cristal, 0.0),                          // potenciais zerados
             W(W_param),                                     // largura da desordem
             N(tam_cristal),                                 // tam. do cristal
@@ -57,9 +65,9 @@ public:
     /**
      * @brief Overloading do método "evoluir()" -- possibilita a definição de um valor padrão para t_max;
      *
-     * @param t_fotos Vetor com instantes de tempo para serem realizadas as "fotografias" de psi
+     * @param t_fotos Array com instantes de tempo para serem realizadas as "fotografias" de psi
      */
-    void evoluir(vector<float> t_fotos)
+    void evoluir(double t_fotos[])
     {
         float t_max = N / 5.0; // maior tempo possível que não gera efeitos de borda
         evoluir(t_fotos, t_max);        // executa a versão principal com o t_max recém definido
@@ -68,10 +76,15 @@ public:
     /**
      * @brief Resolve por Runge-Kutta de 4ª ordem a eq. de Schrödinger do sistema
      *
-     * @param t_fotos Vetor com instantes de tempo para serem realizadas as "fotografias" de psi
+     * @param t_fotos Array com instantes de tempo para serem realizadas as "fotografias" de psi
      * @param t_max Tempo máximo de execução da simulação.
      */
-    void evoluir(vector<float> t_fotos, float t_max) {
+    void evoluir(double t_fotos[], float t_max) {
+        // TODO: Implementar as fotografias nos tempos específicos
+        
+        // passo runge-kutta
+        const double h = 10E-3;
+
         // Inicializa arquivos para escrita de resultados 
         ofstream r;     // Retorno
         ofstream pm;    // Centróide / Posição média 
@@ -95,13 +108,22 @@ public:
         // Abre os arquivos com os nomes predefinidos
 
         r.open(nome_r);
-        pm.open(nome_pm);
         pt.open(nome_pt);
+        pm.open(nome_pm);
         d.open(nome_d);
 
-        // TODO: Adicionar método de distribuição inicial (cond. gaussiana vs delta) aqui
+        float t = 0.0; 
 
-        // TODO: Adicionar método do Runge-Kutta aqui
+        while (t <= t_max)
+        {
+            RK4(h);
+            r << format("{:.5f}    {:.5f}\n", t, calcRetorno());
+            pt << format("{:.5f}    {:.5f}\n", t, calcPart());
+            pm << format("{:.5f}    {:.5f}\n", t, calcPM());
+            d << format("{:.5f}    {:.5f}\n", t, calcDP());
+
+            t += h;
+        };
 
         // Fecha os arquivos e salva os dados
 
@@ -109,9 +131,63 @@ public:
         pm.close();
         pt.close();
         d.close();
-
-
     };
+
+    void condInicial()
+    {
+        if (sigmaDist != 0)
+        {
+            for (int n = 0; n <= N; n++)
+            {
+                k1[n] = exp((-(pow(fabs(n - N/2), 2.0))) / 2.0 * sigmaDist) / (sqrt(2.0 * PI)) + 0.0 * I;
+                k2[n] = exp((-(pow(fabs(n - N/2), 2.0))) / 2.0 * sigmaDist) / (sqrt(2.0 * PI)) + 0.0 * I;
+                k3[n] = exp((-(pow(fabs(n - N/2), 2.0))) / 2.0 * sigmaDist) / (sqrt(2.0 * PI)) + 0.0 * I;
+                k4[n] = exp((-(pow(fabs(n - N/2), 2.0))) / 2.0 * sigmaDist) / (sqrt(2.0 * PI)) + 0.0 * I;
+                psi_n[n] = exp((-(pow(fabs(n - N/2), 2.0))) / 2.0 * sigmaDist) / (sqrt(2.0 * PI)) + 0.0 * I;
+            }
+        }
+        else
+        {
+            psi_n[N/2] = complex<double>(1.0, 0.0);
+        }
+    }
+
+    void RK4(double h)
+    {
+
+        for (int n = 1; n < N; n++)
+        {
+            k1[n] = h * dpsi_dt(psi_n[n+1],
+                                psi_n[n-1],
+                                psi_n[n],
+                                pot[n]);
+        }
+        for (int n = 1; n < N; n++)
+        {
+            k2[n] = h * dpsi_dt(psi_n[n+1] + 0.5 * k1[n+1],
+                                psi_n[n-1] + 0.5 * k1[n-1],
+                                psi_n[n] + k1[n],
+                                pot[n]);
+        }
+        for (int n = 1; n < N; n++)
+        {
+            k3[n] = h * dpsi_dt(psi_n[n+1] + 0.5 * k2[n+1],
+                                psi_n[n-1] + 0.5 * k2[n-1],
+                                psi_n[n] + 0.5 * k2[n],
+                                pot[n]);
+        }
+        for (int n = 1; n < N; n++)
+        {
+            k4[n] = h * dpsi_dt(psi_n[n+1] + k3[n+1],
+                                psi_n[n-1] + k3[n-1],
+                                psi_n[n] + k3[n],
+                                pot[n]);
+        }
+        for (int n = 0; n < N; n++)
+        {
+            psi_n[n] = psi_n[n] + (1.0/6.0) * (k1[n] + 2.0 * k2[n] + 2.0 * k3[n] + k4[n]);
+        }
+    }
 
     /**
      * @brief Povoa o cristal com os potenciais retirados do intervalo preestabelecido (W)
@@ -131,10 +207,10 @@ public:
         uniform_real_distribution<> dist(W_inf, W_sup);     
 
         // atribuição aleatória de potenciais aos átomos da cadeia de acordo com o W escolhido
-        for (int i = 0; i < N; i++)
+        for (int n = 0; n < N; n++)
         {
             // gera e atribui ao i-ésimo sítio um determinado potencial on-site 
-            pot[i] = dist(rng);
+            pot[n] = dist(rng);
         }
     };
 
@@ -155,28 +231,59 @@ public:
         return -I * (psi_anterior + psi_posterior + pot_atual * psi_atual);
     };
 
-    float calcRetorno(const complex<double> psi_n2)
+    float calcRetorno()
     {
-
+        return norm(psi_n[N/2]);
     };
 
-    float calcPart(const complex<double> psi_n)
+    float calcPart()
     {
+        double acm_mod_quad, acm_mod_qutr = 0;
 
+        for (int n = 0; n < N; n++)
+        {
+            acm_mod_quad = acm_mod_quad + norm(psi_n[n]);
+            acm_mod_qutr = acm_mod_qutr + pow(norm(psi_n[n]), 2.0);
+        }
+        
+        return acm_mod_quad / acm_mod_qutr;
     };
 
-    float calcDP(const complex<double> psi_n)
+    float calcDP()
     {
+        float desvio = 0.0;
+        float media = calcPM();
 
+        for (int n = 0; n < N; n++)
+        {
+            desvio = desvio + (pow(n - media, 2.0)) * norm(psi_n[n]); 
+        }
+
+        return desvio; 
     };
 
-    float calcPM(const complex<double> psi_n)
+    float calcPM()
     {
+        float media = 0.0; 
 
+        for (int n = 0; n < N; n++)
+        {
+            media = media + n * norm(psi_n[n]);
+        }
+
+        return media; 
     };
 };
 
 int main()
 {
-    // TODO: Montar o main com o compacto algoritmo de execução
+    // Inicializa o contador temporal 
+    double timecont1 = time(NULL), timecont2; 
+    double t_fotos[] = {0.0, 10.0, 20.0, 30.0, 40.0};
+
+    Cristal cristal(100, 0.0, 0.0);
+
+    cristal.condInicial();
+    cristal.gerarPotenciais();
+    cristal.evoluir(t_fotos);
 }
